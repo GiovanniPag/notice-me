7a8334830683d2067dd54404efb30a8698c9ea18
paganelli <paganelli.giovanni.97@gmail.com>
2021-05-26 14:10:22 +0200

    update detail dialog

--- src/main/webapp/app/entities/note/detail/note-detail-dialog.component.ts
+++ src/main/webapp/app/entities/note/detail/note-detail-dialog.component.ts
@@ -154,8 +154,8 @@ export class NoteDetailDialogComponent implements OnInit {
       id: note.id,
       title: note.title,
       content: note.content,
-      date: note.date ? note.date.format(DATE_TIME_FORMAT) : null,
-      alarm: note.alarm ? note.alarm.format(DATE_TIME_FORMAT) : null,
+      date: note.lastUpdateDate ? note.lastUpdateDate.format(DATE_TIME_FORMAT) : null,
+      alarm: note.alarmDate ? note.alarmDate.format(DATE_TIME_FORMAT) : null,
       status: note.status,
       owner: note.owner,
       tags: note.tags,
@@ -198,8 +198,10 @@ export class NoteDetailDialogComponent implements OnInit {
       id: this.editForm.get(['id'])!.value,
       title: this.editForm.get(['title'])!.value,
       content: this.editForm.get(['content'])!.value,
-      date: this.editForm.get(['date'])!.value ? dayjs(this.editForm.get(['date'])!.value, DATE_TIME_FORMAT) : undefined,
-      alarm: this.editForm.get(['alarm'])!.value ? dayjs(this.editForm.get(['alarm'])!.value, DATE_TIME_FORMAT) : undefined,
+      lastUpdateDate: this.editForm.get(['lastUpdateDate'])!.value
+        ? dayjs(this.editForm.get(['lastUpdateDate'])!.value, DATE_TIME_FORMAT)
+        : undefined,
+      alarmDate: this.editForm.get(['alarmDate'])!.value ? dayjs(this.editForm.get(['alarmDate'])!.value, DATE_TIME_FORMAT) : undefined,
       status: this.editForm.get(['status'])!.value,
       owner: this.editForm.get(['owner'])!.value,
       tags: this.editForm.get(['tags'])!.value,
